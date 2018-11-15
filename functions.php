b20d8a9cc63603a0593e05bdbcfc7dfd41f67659
Tudor <tudor.mardar@gmail.com>
2018-11-15 21:33:34 +0200

    PR-remove-projects-divi-theme

--- functions.php
+++ functions.php
@@ -29,6 +29,17 @@ function custom_styles()
 // Include templates -----------------------------------------
 
 
+// Remove projects post type from Divi theme
+add_filter( 'et_project_posttype_args', 'mytheme_et_project_posttype_args', 10, 1 );
+function mytheme_et_project_posttype_args( $args ) {
+    return array_merge( $args, array(
+        'public'              => false,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => false,
+        'show_in_nav_menus'   => false,
+        'show_ui'             => false
+    ));
+}
 
 // Remove unnecessary features from parent theme function - clean HEAD
 function et_remove_support_theme() {
